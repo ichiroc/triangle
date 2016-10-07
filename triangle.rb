7337bfb20da369b4d670a00bbbb8a5d710bce8ac
NAKATANI Ichiro <nobitahouse@gmail.com>
2016-10-07 11:08:24 +0900

    triangle.rb 完成

---
+++ triangle.rb
@@ -0,0 +1,30 @@
+# coding: utf-8
+# frozen_string_literal: true
+class TriangleTypeDetector
+  def self.run(*args)
+    return '三角形じゃないです＞＜' unless triangle?(*args)
+    return '正三角形ですね！' if regular_triangle?(*args)
+    return '二等辺三角形ですね！' if isosceles_triangle?(*args)
+    '不等辺三角形ですね！'
+  end
+
+  def self.triangle?(*args)
+    return false unless args.size == 3
+    a, b, c = args
+    a + b > c &&
+      b + c > a &&
+      a + c > b
+  end
+
+  def self.isosceles_triangle?(*args)
+    triangle?(*args) && args.uniq.size == 2
+  end
+
+  def self.regular_triangle?(*args)
+    triangle?(*args) && args.uniq.size == 1
+  end
+
+  private_class_method :triangle?, :isosceles_triangle?, :regular_triangle?
+end
+
+print TriangleTypeDetector.run(*ARGV.map(&:to_i)) if ARGV.size.positive?
